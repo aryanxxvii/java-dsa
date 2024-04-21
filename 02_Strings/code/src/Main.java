97c8eaae465ddb55d7ca433e2f18da53e0a5da24
Aryan Wadhawan <aryan.xxvii@gmail.com>
2024-04-21 15:28:41 +0530

    strings

--- 02_Strings/code/src/Main.java
+++ 02_Strings/code/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        String Equality
+
+        String x = "Aryan";
+        String y = "Aryan";
+        String z = new String("Aryan");
+
+        System.out.println(x == y);
+        System.out.println(x == z);
+        System.out.println(x.equals(z));
+
+//    StringBuilder
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Aryan"); // Aryan
+        builder.setCharAt(0, 'X'); // Xryan - same object
+        System.out.println(builder.toString());
+
     }
 }
