76bf5c87701a600b56780eb0f4e4426888ae9f46
Aryan Wadhawan <aryan.xxvii@gmail.com>
2024-04-30 10:54:17 +0530

    Updated NextPermutation.java

--- 04_Arrays/code/src/Medium/NextPermutation.java
+++ 04_Arrays/code/src/Medium/NextPermutation.java
@@ -4,38 +4,51 @@ import java.util.Arrays;
 
 public class NextPermutation {
     public static void main(String[] args) {
-        int[] arr1 = {2,1,3,1,4}; // 2 3 1
-        int[] arr2 = {3,2,1}; // 1 2 3
-        optimal(arr1);
+        int[] arr1 = {1, 3, 2}; // 2 1 3
+        int[] arr2 = {3,2,1};
+        int[] arr3 = {5, 4, 3, 1, 2, 3, 0};
+        int[] ans = optimal(arr3);
+        System.out.println(Arrays.toString(ans));
     }
 
     public static int[] optimal(int[] arr) {
-        // Find the element till where next array will remain same
-        // That element will be the last pivot element
-        // 5 4 3 1p 2 3
-        // 3 is the pivot
-        // Now swap this with the smallest number to the right of pivot: 1
-        // 5 4 3 2p 3 1
-        // Now sort the array to the right of pivot
-        // ANS: 5 4 3 2 1 3
+        int pivot = -1;
 
-        int[] ans = new int[arr.length];
-        int pivot = 0;
-
-        for (int i = 0; i < arr.length - 1; i++) {
+        // Find pivot value => if value at i is less than next value, it is pivot,
+        for (int i = 0; i < arr.length - 1;  i++) {
             if (arr[i] < arr[i+1]) {
                 pivot = i;
             }
         }
-        System.out.println(pivot);
-        reverseToRight(arr, pivot);
-        Arrays.sort(arr, pivot, arr.length-1);
 
-        for (int i : arr) {
-            System.out.print(i + " ");
+
+
+        // If no pivot, reverse whole array and return
+        if (pivot == -1) {
+            reverseToRight(arr, 0);
+            return arr;
         }
-        System.out.println();
-        return ans;
+
+        // Find next greatest element to swap with pivot value
+        int nextGreatest = Integer.MAX_VALUE;
+        int nextGreatestIndex = pivot;
+        for (int i = pivot; i < arr.length; i++) {
+            if (arr[i] > arr[pivot]) {
+                if (arr[i] < nextGreatest) {
+                    nextGreatest = arr[i];
+                    nextGreatestIndex = i;
+                }
+            }
+        }
+        int temp = arr[nextGreatestIndex];
+        arr[nextGreatestIndex] = arr[pivot];
+        arr[pivot] = temp;
+
+
+
+        // Reverse right half of pivot
+        reverseToRight(arr, pivot + 1);
+        return arr;
     }
 
 
