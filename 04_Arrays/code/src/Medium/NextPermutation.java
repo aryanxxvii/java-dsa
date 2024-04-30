af2945cd4e64634d1788fab0230cd791f7815cc9
Aryan Wadhawan <aryan.xxvii@gmail.com>
2024-04-30 12:55:40 +0530

    Updated NextPermutation.java

--- 04_Arrays/code/src/Medium/NextPermutation.java
+++ 04_Arrays/code/src/Medium/NextPermutation.java
@@ -1,67 +1,67 @@
 package Medium;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class NextPermutation {
     public static void main(String[] args) {
-        int[] arr1 = {1, 3, 2}; // 2 1 3
-        int[] arr2 = {3,2,1};
-        int[] arr3 = {5, 4, 3, 1, 2, 3, 0};
-        int[] ans = optimal(arr3);
-        System.out.println(Arrays.toString(ans));
+        int[] arr = {1, 2, 3, 6, 5, 4};
+        int N = arr.length;
+
+        List<Integer> ans = nextPermutation(N, arr);
+        System.out.println(ans);
+    }
+    static List<Integer> nextPermutation(int N, int[] arr){
+        // Find pivot
+        int pivotIndex = findPivotIndex(arr);
+        // If no pivot: reverse whole array
+        if (pivotIndex == -1) {
+            Arrays.sort(arr);
+        }
+        // If pivot:
+        else {
+
+            // get next greatest element
+            int indexNextGreatest = findNextGreatestIndex(arr, pivotIndex);
+
+            // swap with pivot element with next greatest element
+            int temp = arr[pivotIndex];
+            arr[pivotIndex] = arr[indexNextGreatest];
+            arr[indexNextGreatest] = temp;
+
+            // sort all to right of pivot
+            Arrays.sort(arr, pivotIndex + 1, arr.length);
+
+        }
+        List<Integer> ansList = new ArrayList<>();
+
+        for (int i : arr) {
+            ansList.add(i);
+        }
+
+        return ansList;
     }
 
-    public static int[] optimal(int[] arr) {
-        int pivot = -1;
-
-        // Find pivot value => if value at i is less than next value, it is pivot,
-        for (int i = 0; i < arr.length - 1;  i++) {
-            if (arr[i] < arr[i+1]) {
-                pivot = i;
+    // Gives index of pivot: defaults to -1
+    public static int findPivotIndex(int[] arr) {
+        for (int i = arr.length-1; i > 0; i--) {
+            if (arr[i] > arr[i-1]) {
+                return i-1;
             }
         }
-
-
-
-        // If no pivot, reverse whole array and return
-        if (pivot == -1) {
-            reverseToRight(arr, 0);
-            return arr;
-        }
-
-        // Find next greatest element to swap with pivot value
-        int nextGreatest = Integer.MAX_VALUE;
-        int nextGreatestIndex = pivot;
-        for (int i = pivot; i < arr.length; i++) {
-            if (arr[i] > arr[pivot]) {
-                if (arr[i] < nextGreatest) {
-                    nextGreatest = arr[i];
-                    nextGreatestIndex = i;
-                }
-            }
-        }
-        int temp = arr[nextGreatestIndex];
-        arr[nextGreatestIndex] = arr[pivot];
-        arr[pivot] = temp;
-
-
-
-        // Reverse right half of pivot
-        reverseToRight(arr, pivot + 1);
-        return arr;
+        return -1;
     }
 
-
-    // UTILITY FUNCTIONS
-    public static void reverseToRight(int[] arr, int start) {
-        int end = (arr.length - 1);
-        int mid = (start + end)/2;
-        while (start <= mid) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
+    public static int findNextGreatestIndex(int[] arr, int pivotIndex) {
+        int currNextGreatest = Integer.MAX_VALUE;
+        int indexNextGreatest = pivotIndex;
+        for (int i = pivotIndex + 1; i < arr.length; i++) {
+            if (arr[i] > arr[pivotIndex] && arr[i] < currNextGreatest) {
+                currNextGreatest = arr[i];
+                indexNextGreatest = i;
+            }
         }
+        return indexNextGreatest;
     }
 }
