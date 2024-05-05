98eef04236421c7546840c03d78222864e8301c0
Aryan Wadhawan <aryan.xxvii@gmail.com>
2024-05-05 18:01:00 +0530

    Patterns

--- 06_Recursion/code/src/_04_Patterns.java
+++ 06_Recursion/code/src/_04_Patterns.java
@@ -1,2 +1,42 @@
 public class _04_Patterns {
+    public static void main(String[] args) {
+        triangle(5, 0);
+        invertedTriangle(5, 0);
+    }
+
+    public static void triangle(int r, int c) {
+        /*
+            Print (n = 4):
+            * * * *
+            * * *
+            * *
+            *
+         */
+        if (r == 0) {
+            return;
+        }
+
+        if (c < r) {
+            System.out.print("* ");
+            triangle(r, c+1);
+        } else {
+            System.out.println();
+            triangle(r-1, 0);
+        }
+    }
+
+    public static void invertedTriangle(int r, int c) {
+        if (r == 0) {
+            return;
+        }
+
+        if (c < r) {
+            invertedTriangle(r, c+1);
+            System.out.print("* ");
+        } else {
+            invertedTriangle(r-1, 0);
+            System.out.println();
+        }
+    }
+
 }
