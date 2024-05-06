c019d5d23f50e7b44ec73929a3dd1c09267d0f7d
Aryan Wadhawan <aryan.xxvii@gmail.com>
2024-05-06 10:16:27 +0530

    Patterns

--- 06_Recursion/code/src/_04_Patterns.java
+++ 06_Recursion/code/src/_04_Patterns.java
@@ -11,6 +11,14 @@ public class _04_Patterns {
             * * *
             * *
             *
+
+            Decide the placement of the recursive call by looking at the output.
+            Here, first we print N-Stars, so the value of N becomes smaller after each call.
+            So, first print N-Stars, then call the recursive call for N-1.
+
+            If we were to print inverted triangle, first we would have printed 1 Star, then 2 Stars ... then N stars.
+            Here the value of N should first go from N to 1 from the recursive calls, then for printing, 1 to N.
+            So, first call the recursive call for N-1, then start printing from down to up.
          */
         if (r == 0) {
             return;
