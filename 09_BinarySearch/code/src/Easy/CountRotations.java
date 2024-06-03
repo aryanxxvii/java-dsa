9b3b9e98e140e8095cbbf5335d5664c2aadbbadf
Aryan Wadhawan <aryan.xxvii@gmail.com>
2024-06-04 00:37:29 +0530

    Binary Search

---
+++ 09_BinarySearch/code/src/Easy/CountRotations.java
@@ -0,0 +1,7 @@
+package Easy;
+
+public class CountRotations {
+    // find minimum element index
+    // get length of rotated part by comparing with total length
+    // index is the number of rotations
+}
